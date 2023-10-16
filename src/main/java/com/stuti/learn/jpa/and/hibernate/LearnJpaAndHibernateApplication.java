52d90c235505ae4764fd5cfc43ea6f68d3557535
megha <stutimegha2019@gmail.com>
2023-10-16 22:39:27 +0530

    add spring_jdbc

---
+++ src/main/java/com/stuti/learn/jpa/and/hibernate/LearnJpaAndHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.stuti.learn.jpa.and.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJpaAndHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJpaAndHibernateApplication.class, args);
+	}
+
+}
--- src/main/java/com/stuti/learn/jpa/and/hibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ src/main/java/com/stuti/learn/jpa/and/hibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -1,2 +1,16 @@
-package com.stuti.learn.jpa.and.hibernate.course.jdbc;public class CourseJdbcCommandLineRunner {
+package com.stuti.learn.jpa.and.hibernate.course.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CourseJdbcCommandLineRunner implements CommandLineRunner {
+
+    @Autowired
+    private CourseJdbcRepository courseJdbcRepository;
+    @Override
+    public void run(String... args) throws Exception {
+        courseJdbcRepository.insert();
+    }
 }
